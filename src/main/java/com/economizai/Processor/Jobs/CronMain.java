36018200d5bcae0605f143a67d5a73dfc5a45c77
Davyd Cardoso <davydkewen@gmail.com>
2024-02-28 13:36:23 -0300

    feat: novas funcoes de processamento de filas

--- src/main/java/com/economizai/Processor/Jobs/CronMain.java
+++ src/main/java/com/economizai/Processor/Jobs/CronMain.java
@@ -1,29 +1,19 @@
 package com.economizai.Processor.Jobs;
 
 import com.economizai.Processor.Jobs.Processor.ProcessRecurrentTransactions;
-import com.economizai.Processor.Queues.services.RedisMessagePublisher;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnection;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalTime;
 
 @Component
 @EnableScheduling
 public class CronMain {
 
-    @Autowired
-    private RedisMessagePublisher redisMessagePublisher;
 
-    @Scheduled(cron = "1 * * * * *")
+    @Scheduled(cron = "30 8 * * * *")
     public void checkPerHour() {
-//        new ProcessRecurrentTransactions().run();
-
-        redisMessagePublisher.publish("HELLO");
-
+        new ProcessRecurrentTransactions().run();
     }
 
 }
--- src/main/java/com/economizai/Processor/Queues/services/RedisMessageSubscriber.java
+++ src/main/java/com/economizai/Processor/Queues/services/RedisMessageSubscriber.java
@@ -1,10 +1,13 @@
 package com.economizai.Processor.Queues.services;
 
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class RedisMessageSubscriber implements MessageListener {
@@ -14,5 +17,9 @@ public class RedisMessageSubscriber implements MessageListener {
     @Override
     public void onMessage(Message message, byte[] pattern) {
         logger.info("Receive message: {}", message);
+
+        JSONObject json = new JSONObject(message.toString());
+
+        System.out.println(json.get("task"));
     }
 }
